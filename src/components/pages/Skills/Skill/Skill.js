5c587a05d3cbf163b35d06d6f1c2e7de71793a45
aadi350 <a.aadi380@gmail.com>
2021-12-25 18:17:41 -0400

    complete skills page

--- src/components/pages/Skills/Skill/Skill.js
+++ src/components/pages/Skills/Skill/Skill.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './Skill.scss';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 const Skill = (props) => {
     return (
         <div class="subskillcontainer">
-            <img class="subskillimage" src={props.img}/>
+          <FontAwesomeIcon icon={props.img} />
             <div class="subskillname">
         {props.name}
         </div>
--- src/components/pages/Skills/Skills.js
+++ src/components/pages/Skills/Skills.js
@@ -3,18 +3,24 @@ import Skill from './Skill/Skill';
 import './Skills.scss';
 import Particle from "react-particles-js";
 import particlesConfig from "../../../assets/particlesConfig.json";
-
+import {faBroom} from '@fortawesome/free-solid-svg-icons';
+import { faDatabase, faTornado } from '@fortawesome/free-solid-svg-icons';
 
 const Skills = () => {
+
     return (
+        <>
+
         <div class="skillscontainer">
 
-            <Particle params={particlesConfig} className="particles" />
             <div class="skillcontainer">
                 <div class="projectdetails">
                     <div class="skillname">Data Wrangling</div>
                     <hr></hr>
-                    <Skill name='Skill name' img='https://www.kurin.com/wp-content/uploads/placeholder-square.jpg' />
+                  <Skill name='Data Cleansing' />
+                    <Skill name='SQL'  />
+                    <Skill name='ETL Tasks'  />
+                    <Skill name='Big Data Handling' img='https://www.kurin.com/wp-content/uploads/placeholder-square.jpg' />
                 </div>
 
             </div>
@@ -22,24 +28,34 @@ const Skills = () => {
                 <div class="projectdetails">
                     <div class="skillname">Machine Learning</div>
                     <hr></hr>
-                    <Skill name='Skill name' img='https://www.kurin.com/wp-content/uploads/placeholder-square.jpg' />
+                    <Skill name='Risk Modeling' img='https://www.kurin.com/wp-content/uploads/placeholder-square.jpg' />
+                    <Skill name='Multivariate Classification' img='https://www.kurin.com/wp-content/uploads/placeholder-square.jpg' />
+                    <Skill name='Churn Prediction' img='https://www.kurin.com/wp-content/uploads/placeholder-square.jpg' />
+                    <Skill name='Customer Segmentation' img='https://www.kurin.com/wp-content/uploads/placeholder-square.jpg' />
                 </div>
             </div>
             <div class="skillcontainer">
                 <div class="projectdetails">
                     <div class="skillname">Deep Learning</div>
                     <hr></hr>
-                    <Skill name='Skill name' img='https://www.kurin.com/wp-content/uploads/placeholder-square.jpg' />
+                    <Skill name='Chatbot Deployment' img='https://www.kurin.com/wp-content/uploads/placeholder-square.jpg' />
+                    <Skill name='Recommendation Engines' img='https://www.kurin.com/wp-content/uploads/placeholder-square.jpg' />
+                    <Skill name='Image Classification' img='https://www.kurin.com/wp-content/uploads/placeholder-square.jpg' />
                 </div>
             </div>
             <div class="skillcontainer">
                 <div class="projectdetails">
                     <div class="skillname">Data Viz</div>
                     <hr></hr>
-                    <Skill name='Skill name' img='https://www.kurin.com/wp-content/uploads/placeholder-square.jpg' />
+                    <Skill name='Dashboarding' img='https://www.kurin.com/wp-content/uploads/placeholder-square.jpg' />
+                    <Skill name='Interactive Visualization' img='https://www.kurin.com/wp-content/uploads/placeholder-square.jpg' />
+                    <Skill name='Prescriptive Analytics' img='https://www.kurin.com/wp-content/uploads/placeholder-square.jpg' />
                 </div>
             </div>
         </div>
+
+            <Particle params={particlesConfig} className="particles" />
+        </>
     );
 }
 
